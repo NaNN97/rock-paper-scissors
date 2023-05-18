9d6e3c00b3a158e716572d17ae469a6484513a69
NaNN97 <danielbuszta@gmail.com>
2023-05-18 20:03:47 +0100

    playRound

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function (
-  
-)
+function playRound(playerSelection, computerSelection) {
+  // code
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
